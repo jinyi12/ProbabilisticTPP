970ff0bbb9fe2f9af66f3f8bb2d487e7bee9bdb2
Michelle Qiu <michelle.qiu@duke.edu>
2024-12-04 02:25:38 -0500

    added vae decoder

--- models/__init__.py
+++ models/__init__.py
@@ -1,5 +1,6 @@
 from .encoders.gru import GRUTPPEncoder
 from .decoders.rmtpp import RMTPPDecoder, RMTPPLoss
+from .decoders.vaetpp import VAETPPDecoder, VAETPPLoss
 from torch import nn
 
 
@@ -30,3 +31,34 @@ class TPPModel(nn.Module):
             batch["sequence_length"],
         )
         return time_loss, mark_loss, total_loss
+    
+
+class VAETPPModel(nn.Module):
+    def __init__(self, config, hidden_dim, latent_dim, mlp_dim, device):
+        super(VAETPPModel, self).__init__()
+        self.encoder = GRUTPPEncoder(config, hidden_dim=hidden_dim)
+        self.decoder = VAETPPDecoder(
+            hidden_dim=hidden_dim,
+            latent_dim = latent_dim,
+            num_event_types=config.num_event_types,
+            mlp_dim=mlp_dim,
+            device=device,
+        )
+        self.criterion = VAETPPLoss(
+            device=device, ignore_index=config.pad_token_id, decoder=self.decoder
+        )
+
+    def forward(self, batch):
+        hidden_states = self.encoder(batch)
+        decoder_output = self.decoder(hidden_states)
+        return decoder_output
+
+    def compute_loss(self, batch, decoder_output):
+        time_loss, mark_loss, elbo = self.criterion(
+            decoder_output,
+            batch["time_delta_seqs"],
+            batch["type_seqs"],
+            batch["sequence_length"],
+        )
+        return time_loss, mark_loss, elbo
+
